d179b4a26d485bc2f54ab4e2051c210ee07c471b
Akshat <akshatkumar120304@gmail.com>
2025-09-19 09:06:59 +0530

    Updates #1

---
+++ backend/internal/database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"context"
+
+	"github.com/Akshatkm1204/go-boilerplate/backend"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog"
+)
+
+type Database struct {
+	Pool *pgxpool.Pool
+	log  *zerolog.Logger
+}
+
+type multiTracer struct {
+	tracers []any
+}
+
+func (mt *multiTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryStart(context.Context, *pgx.Conn, pgx.TraceQueryStartData) context.Context
+		}); ok {
+			ctx = t.TraceQueryStart(ctx, conn, data)
+		}
+	}
+	return ctx
+}
+
+func (mt *multiTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryEnd(context.Context, *pgx.Conn, pgx.TraceQueryEndData)
+		}); ok {
+			t.TraceQueryEnd(ctx, conn, data)
+		}
+	}
+}
+
+const DatabasePingTimeout = 10
